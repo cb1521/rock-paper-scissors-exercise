cd5db1ee6b3bb8a4985d2d28717183496a6373b1
cb1521 <77860675+cb1521@users.noreply.github.com>
2021-02-03 16:39:59 -0600

    Ask user for an input

--- game.py
+++ game.py
@@ -1,3 +1,44 @@
 # game.py
 
-print("Rock, Paper, Scissors, Shoot!")
+
+print("-------------------")
+print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
+print("-------------------")
+
+#
+#asking a user for an input
+#
+
+x=input("Please choose either 'rock', 'paper', or 'scissors': ")
+
+#print(x)
+print("You chose:",x)
+
+#string interpolation
+#print(f"You chose: {x}")
+
+
+
+
+
+
+exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("You chose: 'rock'")
+print("The computer chose: 'paper'")
+print("-------------------")
+print("Oh, the computer won. It's ok.")
+print("-------------------")
+print("Thanks for playing. Please play again!")
